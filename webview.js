7a31ea482ac35cc9dff590641fe6ddbf8e2585df
Stefan Malzner <stefan@adlk.io>
2018-03-12 12:24:09 +0100

    initial commit

---
+++ webview.js
@@ -0,0 +1,27 @@
+module.exports = (Franz) => {
+  const getMessages = function getMessages() {
+    const muteSelector = '.DQy0Rb';
+
+    // get unread messages
+    let directCount = 0;
+    document.querySelectorAll('.eM5l9e.FVKzAb').forEach((node) => {
+      // Hangouts Chat overrides the muted indicator when there is a direct mention
+      if (!node.closest('content[role="listitem"]').querySelector(muteSelector)) {
+        directCount += 1;
+      }
+    });
+    let indirectCount = 0;
+    document.querySelectorAll('.PL5Wwe.H7du2 .t5F5nf').forEach((node) => {
+      if (!node.closest('content[role="listitem"]').querySelector(muteSelector)) {
+        indirectCount = +1;
+      }
+    });
+    indirectCount -= directCount;
+
+    // set Franz badge
+    Franz.setBadge(directCount, indirectCount);
+  };
+
+  // check for new messages every second and update Franz badge
+  Franz.loop(getMessages);
+};
