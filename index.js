835532cae6fb765b7d2cd1fd29208d6605daca8b
HarderBetterFasterStronger <sfinkel@aspiration.com>
2020-01-07 21:10:26 -0800

    change user-agent to firefox to get around google login issue: This browser or app may not be secure. Learn more.  Try using a different browser

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 // just pass through Franz
-module.exports = Franz => Franz;
+module.exports = Franz =>
+  class HangoutsChat extends Franz {
+    overrideUserAgent() {
+      return "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:71.0) Gecko/20100101 Firefox/71.0";
+    }
+  };
